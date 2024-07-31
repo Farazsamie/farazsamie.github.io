43bf32ed5c3886efe24bc9426634ad62d2776b97
Faraz Samie <farazsamie001@gmail.com>
2024-07-30 17:48:44 -0700

    ghz

---
+++ research-data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        itemName: "Something",
+        itemCategory: "something else",
+        itemLink: "something",
+        itemCopy: "uncvdssuvnnnnunrn",
+        itemImg: "img",
+    },
+    {
+        itemName: "Something",
+        itemCategory: "something else",
+        itemLink: "something",
+        itemCopy: "uncvdssuvnnnnunrn",
+        itemImg: "img",
+    },
+    {
+        itemName: "Something",
+        itemCategory: "something else",
+        itemLink: "something",
+        itemCopy: "uncvdssuvnnnnunrn",
+        itemImg: "img",
+    },
+];
+
+export { data };
