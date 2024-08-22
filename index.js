4f1ae6b68f705d86d9d8a25bb65b9744aafc38db
Faraz Samie <farazsamie001@gmail.com>
2024-08-22 06:19:43 -0700

    2024

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const trails = document.querySelectorAll(".trail");
-const smoothPointer = {
-    x: window.innerWidth / 2,
-    y: window.innerHeight / 2,
-};
+const smoothPointer = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
 
 const totalPointsArray = [40, 35, 30, 25, 20, 15, 10];
+
 window.addEventListener("mousemove", (event) => {
-    gsap.to(smoothPointer, {
-        x: event.clientX,
-        y: event.clientY,
-        duration: 0.5,
-        ease: "power2.out",
-    });
+    smoothPointer.x = event.clientX;
+    smoothPointer.y = event.clientY;
 });
 
-function updatePath () {
+function updatePath() {
     trails.forEach((path, index) => {
-        let points = path.points || [];
-        points.unshift({ ...smoothPointer});
-        while (points.length > totalPointsArray[index]) {
-            points.pop();
+        if (!path.points) path.points = [];
+        path.points.unshift({ ...smoothPointer });
+
+        if (path.points.length > totalPointsArray[index]) {
+            path.points.pop();
         }
-        path.points = points; 
-        
-        if (points.length > 1) {
-            let d = 'M ${points[0].x} ${points[0].y}';
-            for (let i = 1; i < points.length; i++) {
-                d += 'L ${points[i].x} ${pointss[i].y}';
-            }
-            path.setAttribute("d", d);
+
+        let d = `M ${path.points[0].x} ${path.points[0].y}`;
+        for (let i = 1; i < path.points.length; i++) {
+            d += ` L ${path.points[i].x} ${path.points[i].y}`;
         }
-    }); 
+
+        path.setAttribute("d", d);
+    });
 
     requestAnimationFrame(updatePath);
 }
+
 updatePath();
