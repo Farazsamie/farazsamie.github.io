b69a1416b8275868331c303624f15db10ccdc15a
Faraz Samie <farazsamie001@gmail.com>
2024-09-28 13:25:22 -0700

    2024

---
+++ experience.js
@@ -0,0 +1,20 @@
+import {titles} from ".experience-data.js";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    gsap.registerPlugin(CustomEase);
+    CustomeEase.create (
+        "hop",
+        "M0, 0 C0.071, 0.505 0.192, 0.726 0.318, 0.852 0.45, 0.984 0.504, 1 1, 1"
+    );
+
+
+    const sliderNav = document. querySelector (". slider-nav");
+    const slidesContainer = document. querySelector (" slides");
+    const bOverlay = document. querySelector (" . bg-overlay");
+    const slideTitle = document. querySelector (". slide-title");
+    const numberofItems = 30;
+    let currentIndex = 0;
+
+});
+
