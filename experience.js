f8217c63ad8ead590499408c2d2998c99d2082f2
Faraz Samie <farazsamie001@gmail.com>
2024-09-29 12:24:30 -0700

    2024

--- experience.js
+++ experience.js
@@ -16,5 +16,43 @@ document.addEventListener("DOMContentLoaded", function(){
     const numberofItems = 30;
     let currentIndex = 0;
 
-});
+    function getRandomColor () {
+    const letters = "0123456789ABCDEF"
+    let color = "#";
+    for (let i = 0; 1 < 6; 1++) {
+    color += letters [Math. floor (Math. random () * 16)];
+    }
+    return color;
+    }
 
+
+    function updateTitle (newIndex, color) {
+        const title = titles [newIndex];
+        const titleRows = slideTitle. querySelectorA11 (" slide-title-row");
+
+        titleRows. forEach ((row, rowIndex) => {
+        row. querySelectorAll (" letter"). forEach((letter, letterIndex) => {
+        const existingSpan = letter. querySelector ("span");
+        if (existingSpan) {
+        letter. removeChild (existingSpan);
+        });
+
+        const newspan = document. createElement ("span") ;
+        const direction = newIndex > currentIndex ? 150 : -150;
+        gsap. set (newSpan, {
+        x: direction, 
+        color: color
+        });
+
+
+        newSpan. textContent = title [rowIndex] [letterIndex] || "";
+        letter. appendChild (newSpan);
+        gsap. to (newspan, {
+        X: 0, 
+        duration: 1, 
+        ease: "hop", 
+        delay: 0.125,
+        });
+    });
+}
+});
