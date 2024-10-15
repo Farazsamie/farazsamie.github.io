26571b3bff7eb87089ecb18295e884b35c0a7e78
Faraz Samie <farazsamie001@gmail.com>
2024-10-14 23:20:00 -0700

    0ct 14, 2024

--- experience-data.js
+++ experience-data.js
@@ -1,9 +1,10 @@
 const titles = [
-    ["one", "ex 1"],  
-    ["two", "ex 2"],
-    ["three", "ex 3"],
-    ["four", "ex 4"],  // Continue adding entries to match your number of slides
-    // ...add more titles to match the numberOfItems (30)
+    ["Adidas", "Sales Associate at Adidas"],  
+    ["Google", "Software Engineer at Google"],
+    ["Tesla", "Engineer at Tesla"],
+    ["Apple", "Product Manager at Apple"],
+    // Add more entries as needed
 ];
 
 export { titles };
+
