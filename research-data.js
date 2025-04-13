968ffe3e3fdb62c37bf37478e79ed59f77e36458
Faraz Samie <farazsamie001@gmail.com>
2025-04-13 10:16:51 -0700

    n

--- research-data.js
+++ research-data.js
@@ -2,8 +2,8 @@ const data = [
     {
         itemName: "Research Overview: BNPL on Your Credit Card",
         itemDescription: "Category: Research Overview",
-        itemLink: "https://example.com/publication",
         itemCopy: "An in-depth look at BNPL impacts on consumer behavior.", // Optional copy for extra text
+        itemLink: "https://example.com/publication",
         itemImg: "images/RPIMG1.png"
     },
 ];
