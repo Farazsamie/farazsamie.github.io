54a75ae04407817c740707d2959afa9e24cbbb48
Faraz Samie <farazsamie001@gmail.com>
2025-04-13 10:20:22 -0700

    n

--- research-data.js
+++ research-data.js
@@ -4,7 +4,7 @@ const data = [
         itemDescription: "Category: Research Overview",
         itemCopy: "An in-depth look at BNPL impacts on consumer behavior.", // Optional copy for extra text
         itemLink: "https://example.com/publication",
-        itemImg: "images/RPIMG1.png"
+        itemImg: "images/RP1IMG.png"
     },
 ];
 
